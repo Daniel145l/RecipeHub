f2423a8b098d848ba1b4fbf414da419add84dc5e
Daniel Lucas <daniellucasm18@gmail.com>
2025-04-23 00:38:49 -0300

    feat: add route to favorite page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom"
+import { Favorites } from "./pages/Favorites"
 import { Home } from "./pages/Home"
 import RecipeDetails from "./pages/RecipeDetails"
 
@@ -7,6 +8,7 @@ function App() {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
     </Routes>
   )
 }
