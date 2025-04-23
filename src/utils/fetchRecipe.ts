2987e3a9534692f055c70996f2c8c8630e925fc7
Daniel Lucas <daniellucasm18@gmail.com>
2025-04-22 23:13:33 -0300

    feature: creating a new function to fetch recipes by category

--- src/utils/fetchRecipe.ts
+++ src/utils/fetchRecipe.ts
@@ -6,7 +6,7 @@ export const fetchRecipe = async (query: string): Promise<Recipe[]> => {
     const res = await api.get(`/search.php?s=${query}`);
     return res.data.meals || [];
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return [];
   }
 };
