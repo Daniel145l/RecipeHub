fc2be93b71fba9bd3f4636774dde458a02bcbb42
Daniel Lucas <daniellucasm18@gmail.com>
2025-04-27 00:10:41 -0300

    style: add animations with staggerChildren

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { Link } from 'react-router-dom';
@@ -66,6 +67,23 @@ export const Home = () => {
     fetch();
   }, [selectedCategory])
 
+  // framer motion animations
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const cardVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <div className='p-4 flex flex-col justify-center gap-4 lg:px-12'>
       <header className='flex items-center justify-between mb-10 p-6 px-2 rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] gap-2 w-full md:px-6'>
@@ -101,18 +119,24 @@ export const Home = () => {
       </header>
 
       {loading ? (
-        <p className='text-green-700 font-bold'>Carregando receitas</p>
+        <p className='text-green-700 font-bold text-center'>Carregando receitas</p>
       ) : (
-       <div className='w-full mx-auto grid grid-cols-1 px-8 gap-12 text-center lg:grid-cols-2 lg:px-12'>
+       <motion.div className='w-full mx-auto grid grid-cols-1 px-8 gap-12 text-center lg:grid-cols-2 lg:px-12'
+        variants={containerVariants}
+        initial='hidden'
+        animate='visible'
+       >
         {recipes.map((recipe) => (
-          <div >
+          <motion.div
+            variants={cardVariants}
+          >
             <RecipeCard key={recipe.idMeal} recipe={recipe} />
             {/* <Link to={`/recipe/${recipe.idMeal}`} className='bg-[#E74C3C] p-2 text-white rounded-sm'>
               Ver detalhes
             </Link> */}
-          </div>
+          </motion.div>
         ))}
-       </div>
+       </motion.div>
       )}
 
     </div>
